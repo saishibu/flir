8a9b99e81432e77d4bd972a2701274300ec1e027
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 12:30:55 +0530

    Update generatePower.py

--- generatePower.py
+++ generatePower.py
@@ -3,13 +3,18 @@ from syntheticPowerData import generate
 
 conn = pymysql.connect(host='localhost', user='sai', password='sheeba99', database='flir')
 cur = conn.cursor()
-cur.execute("SELECT DevID FROM `DevInfo` ORDER BY `DevInfo`.`ID` ASC")
-DevID = cur.fetchall()
-cur.execute("SELECT seed FROM `DevInfo` ORDER BY `DevInfo`.`ID` ASC")
-seed = cur.fetchall()
+cur.execute("SELECT DevID,maxS,seed FROM `DevInfo` ORDER BY `DevInfo`.`ID` ASC")
+DevInfo = cur.fetchall()
+for row in DevInfo:
+  DevID = DevInfo[0]
+  maxS = DevInfo[1]
+  seed = DevInfo[2]
+
 print(DevID)
+print(maxS)
 print(seed)
-power=generate(DevID[0][0],5000,seed[0][0])
+print(" ")
+power=generate(DevID[1][0],maxS[1][0],seed[1][0])
 print(power)
 print(DevID[0][0])
 cur.execute("INSERT INTO `syntheticPowerData` (`DevID`, `v`, `i`, `pf`, `p`, `q`, `s`, `status`, `seed`) VALUES (%(DevID)s, %(v)s, %(i)s, %(pf)s, %(p)s, %(q)s, %(s)s, %(status)s, %(seed)s);",power);
