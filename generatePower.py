31930d882a803c2e30d5d2dcc003c23038445e89
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 12:16:05 +0530

    Update generatePower.py

--- generatePower.py
+++ generatePower.py
@@ -3,10 +3,10 @@ from syntheticPowerData import generate
 
 conn = pymysql.connect(host='localhost', user='sai', password='sheeba99', database='flir')
 cur = conn.cursor()
-cur.execute("SELECT DevID,seed FROM `DevInfo` ORDER BY `DevInfo`.`ID` ASC")
-DevInfo = cur.fetchone()
-DevID = DevInfo[0]
-seed = DevInfo[1]
+cur.execute("SELECT DevID FROM `DevInfo` ORDER BY `DevInfo`.`ID` ASC")
+DevID = cur.fetchall()
+cur.execute("SELECT seed FROM `DevInfo` ORDER BY `DevInfo`.`ID` ASC")
+seed = cur.fetchall()
 print(DevID)
 print(seed)
 power=generate(1,5000,0)
