4562536677fb638e636e176ea2254f9b42c1e698
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 10:48:55 +0530

    Update syntheticPowerData.py

--- syntheticPowerData.py
+++ syntheticPowerData.py
@@ -1,6 +1,16 @@
 import random, math
 
-def generate():
-pf=round(random.uniform(0.7,1),2)
-ang=math.acos(pf)
-powerData={'v':round(random.uniform(110,230),2),'i':round(random.uniform(0,20),2),'pf':pf,'p':round(v*i*pf,3),'q':round(v*i*math.sin(ang),3),'s':round(math.sqrt((v*v*i*i*pf*pf)+(v*v*i*i*math.sin(ang)*math.sin(ang))),3)}
+def generate(DevID,maxS):
+  pf = round(random.uniform(0.7,1),2)
+  ang = math.acos(pf)
+  v = round(random.uniform(110,230),2)
+  i = round(random.uniform(0,20),2)
+  p = round(v*i*pf,3)
+  q = round(v*i*math.sin(ang),3)
+  s = round(math.sqrt(p*p+q*q),3)
+  status = ""
+  if s>maxS:
+    status = "OL"
+  powerData={'id':DevID,'v':v,'i':i,'pf':pf,'p':p,'q':q,'s':s,'status':status}
+  return(powerData)
+  
