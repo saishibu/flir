d4feca383bee651dfa5985cf1d4842233fdf889f
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 12:43:08 +0530

    Update generatePower.py

--- generatePower.py
+++ generatePower.py
@@ -7,16 +7,16 @@ cur.execute("SELECT DevID,maxS,seed FROM `DevInfo`;")
 DevInfo = cur.fetchall()
 print(DevInfo)
 for row in DevInfo:
-  DevID = DevInfo[0]
-  maxS = DevInfo[1]
-  seed = DevInfo[2]
+  DevID = row[0]
+  maxS = row[1]
+  seed = row[2]
 
 print(DevID)
 print(maxS)
 print(seed)
 print(" ")
-# power=generate(DevID[1][0],maxS[1][0],seed[1][0])
-# print(power)
-# print(DevID[0][0])
-# cur.execute("INSERT INTO `syntheticPowerData` (`DevID`, `v`, `i`, `pf`, `p`, `q`, `s`, `status`, `seed`) VALUES (%(DevID)s, %(v)s, %(i)s, %(pf)s, %(p)s, %(q)s, %(s)s, %(status)s, %(seed)s);",power);
-# conn.commit() 
+power=generate(DevID[1][0],maxS[1][0],seed[1][0])
+print(power)
+print(DevID[0][0])
+cur.execute("INSERT INTO `syntheticPowerData` (`DevID`, `v`, `i`, `pf`, `p`, `q`, `s`, `status`, `seed`) VALUES (%(DevID)s, %(v)s, %(i)s, %(pf)s, %(p)s, %(q)s, %(s)s, %(status)s, %(seed)s);",power);
+conn.commit() 
