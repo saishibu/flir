7e011808c9eca8dee07164a321ed58aa50f55a73
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 11:52:33 +0530

    Update syntheticPowerData.py

--- syntheticPowerData.py
+++ syntheticPowerData.py
@@ -1,6 +1,6 @@
-import random, math
+import random, math, pymysql
 
-def generate(DevID,maxS):
+def generate(DevID,maxS,seed):
   pf = round(random.uniform(0.7,1),2)
   ang = math.acos(pf)
   v = round(random.uniform(110,230),2)
@@ -11,6 +11,6 @@ def generate(DevID,maxS):
   status = ""
   if s>maxS:
     status = "OL"
-  powerData={'id':DevID,'v':v,'i':i,'pf':pf,'p':p,'q':q,'s':s,'status':status}
+  powerData={'id':DevID,'v':v,'i':i,'pf':pf,'p':p,'q':q,'s':s,'status':status,'seed':seed}
   return(powerData)
   
