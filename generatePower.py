6d093143ef82855314ab48afb8dc7cc10771a06d
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 12:44:49 +0530

    Update generatePower.py

--- generatePower.py
+++ generatePower.py
@@ -5,7 +5,7 @@ conn = pymysql.connect(host='localhost', user='sai', password='sheeba99', databa
 cur = conn.cursor()
 cur.execute("SELECT DevID,maxS,seed FROM `DevInfo`;")
 DevInfo = cur.fetchall()
-print(DevInfo)
+# print(DevInfo)
 for row in DevInfo:
   DevID = row[0]
   maxS = row[1]
@@ -15,8 +15,9 @@ print(DevID)
 print(maxS)
 print(seed)
 print(" ")
-power=generate(DevID[1][0],maxS[1][0],seed[1][0])
-print(power)
-print(DevID[0][0])
-cur.execute("INSERT INTO `syntheticPowerData` (`DevID`, `v`, `i`, `pf`, `p`, `q`, `s`, `status`, `seed`) VALUES (%(DevID)s, %(v)s, %(i)s, %(pf)s, %(p)s, %(q)s, %(s)s, %(status)s, %(seed)s);",power);
-conn.commit() 
+
+# power=generate(DevID[1][0],maxS[1][0],seed[1][0])
+# print(power)
+# print(DevID[0][0])
+# cur.execute("INSERT INTO `syntheticPowerData` (`DevID`, `v`, `i`, `pf`, `p`, `q`, `s`, `status`, `seed`) VALUES (%(DevID)s, %(v)s, %(i)s, %(pf)s, %(p)s, %(q)s, %(s)s, %(status)s, %(seed)s);",power);
+# conn.commit() 
