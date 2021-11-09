2a84a1c51110ce20c988be157f2fb2ac296f2ed2
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 12:33:41 +0530

    Update generatePower.py

--- generatePower.py
+++ generatePower.py
@@ -14,8 +14,8 @@ print(DevID)
 print(maxS)
 print(seed)
 print(" ")
-power=generate(DevID[1][0],maxS[1][0],seed[1][0])
-print(power)
-print(DevID[0][0])
-cur.execute("INSERT INTO `syntheticPowerData` (`DevID`, `v`, `i`, `pf`, `p`, `q`, `s`, `status`, `seed`) VALUES (%(DevID)s, %(v)s, %(i)s, %(pf)s, %(p)s, %(q)s, %(s)s, %(status)s, %(seed)s);",power);
-conn.commit() 
+# power=generate(DevID[1][0],maxS[1][0],seed[1][0])
+# print(power)
+# print(DevID[0][0])
+# cur.execute("INSERT INTO `syntheticPowerData` (`DevID`, `v`, `i`, `pf`, `p`, `q`, `s`, `status`, `seed`) VALUES (%(DevID)s, %(v)s, %(i)s, %(pf)s, %(p)s, %(q)s, %(s)s, %(status)s, %(seed)s);",power);
+# conn.commit() 
