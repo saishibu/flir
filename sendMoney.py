1952e59183cf742c1dc24bf1b3dc8564d6c97a72
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 14:00:13 +0530

    Update sendMoney.py

--- sendMoney.py
+++ sendMoney.py
@@ -11,9 +11,9 @@ receiver = rapi.get_new_addresses()
 receiver=receiver['addresses'][0]
 print(receiver)
 tx = ProposedTransaction(address=Address(receiver), message=TryteString.from_unicode("message"),tag=Tag('VALUETX'),value=int(100))
-#input = sapi.get_inputs(start=0, stop=10)
-#inputs = input['inputs']
-tx = sapi.prepare_transfer(transfers=[tx])
+input = sapi.get_inputs(start=0, stop=3)
+inputs = input['inputs']
+tx = sapi.prepare_transfer(transfers=[tx], inputs=inputs)
 process = sapi.send_trytes(tx['trytes'], depth=1, min_weight_magnitude=9)
 #   return "Transaction Success"
 
