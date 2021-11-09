fc323088c2119126dbc0d027d7d7455f40aa4c04
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 13:03:41 +0530

    Update totalBalance.py

--- totalBalance.py
+++ totalBalance.py
@@ -1,3 +1,14 @@
 from iota import Iota, Address, TryteString, ProposedTransaction, Tag
 from iota.crypto.types import Seed
 import time
+url="http://sai-iota.duckdns.org:14265"
+conn = pymysql.connect(host='localhost', user='sai', password='sheeba99', database='flir')
+cur = conn.cursor()
+cur.execute("SELECT seed FROM `DevInfo`;")
+DevInfo=cur.fetchall()
+for row in DevInfo:
+  seed = row[0]
+  api = Iota(url,seed)
+  input = api.get_inputs(start=0, stop=10)
+  totalbalance=input['totalBalance']
+  print(totalbalance)
