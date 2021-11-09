d218552ef0b6cda79d1f716a40717b4d8af4ffcc
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 12:12:30 +0530

    Update generatePower.py

--- generatePower.py
+++ generatePower.py
@@ -1,3 +1,13 @@
+import pymysql
 from syntheticPowerData import generate
+
+conn = pymysql.connect(host='localhost', user='sai', password='sheeba99', database='flir')
+cur = conn.cursor()
+cur.execute("SELECT DevID,seed FROM `DevInfo` ORDER BY `DevInfo`.`ID` ASC")
+DevInfo = cur.fetchall()
+DevID = DevInfo[0]
+seed = DevInfo[1]
+print(DevID)
+print(seed)
 power=generate(1,5000,0)
 print(power)
