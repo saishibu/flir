729eeaaa7581fe7ad620e281762727a9345848e7
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-09 13:09:39 +0530

    Update totalBalance.py

--- totalBalance.py
+++ totalBalance.py
@@ -4,11 +4,15 @@ import time, pymysql
 url="http://sai-iota.duckdns.org:14265"
 conn = pymysql.connect(host='localhost', user='sai', password='sheeba99', database='flir')
 cur = conn.cursor()
-cur.execute("SELECT seed FROM `DevInfo`;")
+cur.execute("SELECT DevID,seed FROM `DevInfo`;")
 DevInfo=cur.fetchall()
 for row in DevInfo:
-  seed = row[0]
+  DevID = row[0]
+  seed = row[1]
   api = Iota(url,seed)
   input = api.get_inputs(start=0, stop=10)
   totalbalance=input['totalBalance']
   print(totalbalance)
+  data={'totalbalance':totalbalance,'DevID':DevID,'seed':seed}
+  cur.execute("INSERT INTO `totalBalance` ( `DevID`, `seed`, `totalbalance`) VALUES (%(DevID)s,%(seed)s,%(totalbalance)s);",data)
+  conn.commit()
