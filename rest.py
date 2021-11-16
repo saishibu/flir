df96b209c76ca800bbb7904b3ce4be115a1a006e
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-16 14:33:20 +0530

    Update rest.py

--- rest.py
+++ rest.py
@@ -1,1 +1,28 @@
+from flask import Flask, jsonify,request
+import os
+from flaskext.mysql import MySQL
 
+from iota import Iota, Address, TryteString, ProposedTransaction, Tag
+from iota.crypto.types import Seed
+
+#assign a Flask Class
+app=Flask(__name__)
+mysql = MySQL()
+mysql.init_app(app)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'sai'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'sheeba99'
+app.config['MYSQL_DATABASE_DB'] = 'flir'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+url="http://sai-iota.duckdns.org:14265"
+
+@app.route('/')
+def welcome():
+	return "Welcome to Hyperlocal eMart, Shopping made simple. \n Login to continue."
+
+if __name__ == '__main__':
+#app.run will make the APIs available on this particular IP address and Port 5000
+#0.0.0.0  ip means any one can access.
+    app.run(host="0.0.0.0",port=8080,debug=1)
