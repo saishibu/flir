147d85cec99e48f5ea40d9e2aa590c816f134c96
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-17 11:13:09 +0530

    Update rest.py

--- rest.py
+++ rest.py
@@ -43,5 +43,6 @@ if __name__ == '__main__':
 #app.run will make the APIs available on this particular IP address and Port 5000
 #0.0.0.0  ip means any one can access.
 	context = ('local.crt', 'local.key')#certificate and key files
-	app.run(debug=True, host="0.0.0.0", port=8080, ssl_context=("cert.pem", "key.pem"))
+	app.run(debug=True, host="0.0.0.0", port=8080)
+# 	ssl_context=("cert.pem", "key.pem")
 #     app.run(host="0.0.0.0",port=8080,debug=1)
