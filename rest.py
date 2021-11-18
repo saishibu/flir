31425505d6d3054bd3c98cc2c0cbcb3464dbeffe
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-18 10:16:16 +0530

    Update rest.py

--- rest.py
+++ rest.py
@@ -5,6 +5,10 @@ from flaskext.mysql import MySQL
 from iota import Iota, Address, TryteString, ProposedTransaction, Tag
 from iota.crypto.types import Seed
 
+import ssl
+context = ssl.SSLContext()
+context.load_cert_chain('cert.pem', 'key.pem')
+
 #assign a Flask Class
 app=Flask(__name__)
 mysql = MySQL()
@@ -16,7 +20,7 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'sheeba99'
 app.config['MYSQL_DATABASE_DB'] = 'flir'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-url="http://sai-iota.duckdns.org:14265"
+url="http://localhost:14265"
 
 @app.route('/')
 def welcome():
@@ -45,6 +49,6 @@ if __name__ == '__main__':
 #app.run will make the APIs available on this particular IP address and Port 5000
 #0.0.0.0  ip means any one can access.
 	context = ('local.crt', 'local.key')#certificate and key files
-	app.run(debug=True, host="0.0.0.0", port=8080,ssl_context=("cert.pem", "key.pem"))
+	app.run(debug=True, host="0.0.0.0", port=8080,ssl_context=context)
 # 	ssl_context=("cert.pem", "key.pem")
 #     app.run(host="0.0.0.0",port=8080,debug=1)
