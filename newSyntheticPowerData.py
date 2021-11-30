a8c8930735571df61469f946146dad6ea6d22fdd
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-30 16:42:41 +0530

    Update newSyntheticPowerData.py

--- newSyntheticPowerData.py
+++ newSyntheticPowerData.py
@@ -20,8 +20,6 @@ else:
   H_ThDER = 5 #Wind Only
   L_ThDER = 0
 PDER = round(random.uniform(L_ThDER,H_ThDER),2)
-print('DER')
-print(PDER)
 
 # 2 - Home With Solar
 # 	Time 12am to 11:59pm
@@ -48,18 +46,12 @@ if PHWSTotal < 0:
 else:
   print("Importing")
 
-print('Home Wo Solar')  
-print(PHWSTotal)
-print(PHWSImport)
-print(PHWSExport)
-
 # 3 - Home Without Solar
 # 	Time 12am to 11:59pm
 # 	Import power from DER
 # 	Consumption Max 3kW
 # 	Import at Rs. 4PU
 PHWOSImport = round(random.uniform(2,3),2)
-print(PHWOSImport)
 
 # 4 - Charger
 # 	Time 12am to 11:59pm
@@ -68,7 +60,6 @@ print(PHWOSImport)
 # 	Max 10kW
 
 Charger = round(random.uniform(3,10),2)
-print(Charger)
 
 # 5 - EV
 # 	Import power from Charger
@@ -76,7 +67,6 @@ print(Charger)
 # 	Max 10kW
 print('EV')
 EV = round(Charger*1.3 ,2)
-print(EV)
 
 # 6 - Factory
 # 	Time 9am to 6pm
@@ -100,27 +90,23 @@ else:
   L_ThFE = 0 #Solar Low Th
   H_ThF = 0 #Import High Th
   L_ThF = 0 #Import Low Th
-print('F')
 FImport = round(random.uniform(L_ThFE,H_ThFE),2)
 FExport = round(random.uniform(L_ThF,H_ThF),2)
 FTotal = round(FImport-FExport,2)
-print(FTotal)
 # 7 - Battery
 # 	Import and Export power from DER
 # 	Import and Export at Rs. 3 PU
 # 	Max capacity - 10kW
-print('B')
+
 BImport = round(random.uniform(0,10),2)
 BExport = round(random.uniform(0,10),2)
 BTotal = round(BImport-BExport,2)
-print(BTotal)
 # 8 - Solar Park
 # 	Time 8am to 6pm
 # 	Capacity - 10kW
 # 	Export power to DER
 # 	Climate affects performance
 # 	Export at Rs. 3 PU
-print('SP')
 if time(10,30) <= datetime.now().time() <= time(16,30):
   H_ThSP = 2 #
   L_ThSP = 0
@@ -129,7 +115,7 @@ else:
   L_ThSP = 0
 
 SPExport = round(random.uniform(L_ThH,H_ThH),2)
-print(SPExport)
 data ={ 'PDER':PDER,'PHWSImport':PHWSImport,'PHWSExport':PHWSExport, 'PHWSTotal':PHWSTotal, 'PHWOSImport':PHWOSImport, 'Charger':Charger, 'EV':EV, 'FImport':FImport, 'FExport':FExport, 'FTotal':FTotal, 'BImport':BImport, 'BExport':BExport, 'BTotal':BTotal, 'SPExport':SPExport }
+print(data)
 cur.execute("INSERT INTO `newSyntheticPowerData` (`PDER`, `PHWSImport`, `PHWSExport`, `PHWSTotal`, `PHWOSImport`, `Charger`, `EV`, `FImport`, `FExport`, `FTotal`, `BImport`, `BExport`, `BTotal`, `SPExport`) VALUES (%(PDER)s, %(PHWSImport)s, %(PHWSExport)s, %(PHWSTotal)s, %(PHWOSImport)s, %(Charger)s, %(EV)s, %(FImport)s, %(FExport)s, %(FTotal)s, %(BImport)s, %(BExport)s, %(BTotal)s, %(SPExport)s );",data)
 conn.commit()
