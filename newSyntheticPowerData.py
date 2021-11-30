b662347a5bf0cd48b03032e19bf4ad1e34c84e95
Sai Shibu N B <saishibu38380@gmail.com>
2021-11-30 16:03:05 +0530

    Update newSyntheticPowerData.py

--- newSyntheticPowerData.py
+++ newSyntheticPowerData.py
@@ -15,7 +15,7 @@ else:
   H_ThDER = 5 #Wind Only
   L_ThDER = 0
 PDER = round(random.uniform(L_ThDER,H_ThDER),2)
-
+print('DER')
 print(PDER)
 
 # 2 - Home With Solar
@@ -29,20 +29,21 @@ print(PDER)
 # 	Export at Rs. 2 PU and Import at Rs. 4 PU
 
 if time(10,30) <= datetime.now().time() <= time(16,30):
-  H_ThDER = 2 #
-  L_ThDER = 0
+  H_ThH = 2 #
+  L_ThH = 0
 else:
-  H_ThDER = 0 
-  L_ThDER = 0
-
-PHWSImport = round(random.uniform(L_ThDER,3),2)
-PHWSExport = round(random.uniform(L_ThDER,H_ThDER),2)
+  H_ThH = 0 
+  L_ThH = 0
+print('Home W solar')
+PHWSImport = round(random.uniform(L_ThH,3),2)
+PHWSExport = round(random.uniform(L_ThH,H_ThH),2)
 PHWSTotal = round(PHWSImport-PHWSExport,2)
 if PHWSTotal < 0:
   print("Exporting")
 else:
   print("Importing")
-  
+
+print('Home Wo Solar')  
 print(PHWSTotal)
 print(PHWSImport)
 print(PHWSExport)
@@ -61,10 +62,16 @@ print(PHWOSImport)
 # 	Import at Rs. 20PU
 # 	Max 10kW
 
+Charger = round(random.uniform(3,10),2)
+print(Charger)
+
 # 5 - EV
 # 	Import power from Charger
 # 	Import at Rs. 25PU
 # 	Max 10kW
+print('EV')
+EV = round(Charger*1.3 ,2)
+print(EV)
 
 # 6 - Factory
 # 	Time 9am to 6pm
@@ -73,14 +80,48 @@ print(PHWOSImport)
 # 	Import at Rs. 10PU
 # 	Max 10kW
 
+if time(9,00) <= datetime.now().time() <= time(10,30):
+  H_ThFE = 0 #Solar High Th
+  L_ThFE = 0 #Solar Low Th
+  L_ThF = 10 #Import High Th
+  L_ThF = 0 #Import Low Th
+elif time(10,31) <= datetime.now().time() <= time(4,30):
+  H_ThFE = 5 #Solar High Th
+  L_ThFE = 0 #Solar Low Th
+  L_ThF = 4.5 #Import High Th
+  L_ThF = 0 #Import Low Th
+else:
+  H_ThFE = 0 #Solar High Th
+  L_ThFE = 0 #Solar Low Th
+  L_ThF = 0 #Import High Th
+  L_ThF = 0 #Import Low Th
+print('F')
+FImport = round(random.uniform(L_ThF,3),2)
+FExport = round(random.uniform(L_ThDFH,H_ThFH),2)
+FTotal = round(FImport-FExport,2)
+print(FTotal)
 # 7 - Battery
 # 	Import and Export power from DER
 # 	Import and Export at Rs. 3 PU
 # 	Max capacity - 10kW
 
+BImport = round(random.uniform(0,10),2)
+BExport = round(random.uniform(0,10),2)
+BTotal = round(BImport-BExport,2)
+print(BTotal)
 # 8 - Solar Park
 # 	Time 8am to 6pm
 # 	Capacity - 10kW
 # 	Export power to DER
 # 	Climate affects performance
 # 	Export at Rs. 3 PU
+print('SP')
+if time(10,30) <= datetime.now().time() <= time(16,30):
+  H_ThSP = 2 #
+  L_ThSP = 0
+else:
+  H_ThSP = 0 
+  L_ThSP = 0
+
+SPExport = round(random.uniform(L_ThH,H_ThH),2)
+print(SPExport)
