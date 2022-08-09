c64cae5999dee5767f800ea969335ee3c0185a5b
Sai Shibu N B <saishibu38380@gmail.com>
2022-08-09 10:35:24 +0530

    Update readExcel.py

---
+++ eth based/truffle/readExcel.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def getDelta(): 
+  excel_data = pd.read_excel('Optimization.xlsx','Sheet2')
+  data = pd.DataFrame(excel_data)
+  delta = {p1:float(data['P1']),p2:float(data['P2']),p3:float(data['P3']),p4:float(data['P4']),p5:float(data['P5'])}
+  return delta
